962c2a7ee57c691a536e3ff7eff2b84c39221ab9
cauaolivv <caua.rodrigues.oliveira@escola.pr.gov.br>
2024-09-26 11:38:31 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
 function calcula(opera) {
-   let num1 = parseFloat(document.getElementById('zeroum'))
-}
+   let num1 = parseFloat(document.getElementById('zeroum').value);
+   let num2 = parseFloat(document.getElementById('zerodois').value);
+   let resultado = 0;
+   if(isNaN(num1) || isNaN(num2)){
+      alert('acorde pra a vida');
+
+   }else{
+
+      if(opera == '+'){
+         resultado = num1 + num2;
+      }else if(opera == '-'){
+        resultado = num1 - num2;
+      }else if(opera == '*'){
+         resultado = num1 * num2;
+      }else if(opera == '/'){
+         resultado = num1 / num2;
+      }
+      document.getElementById('resultado').textContent = 'resultado' + resultado;
+
+    }
+
+
+   }
